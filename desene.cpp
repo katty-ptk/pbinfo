dddcf9c1f135b5415d65e1ca49fb11383ebabdac
Katty Lazar <katy.lazar05@gmail.com>
2021-12-04 10:13:23 +0200

    solved 2nd part of problem - rombul

--- desene.cpp
+++ desene.cpp
@@ -24,16 +24,27 @@ int main() {
 
         case 2:
             // romb
-            for ( int i = 1; i <= 2 * n - 1; i++ ) {
-                for (int j = 1; j <= 2 * n - 1; j++) {
-                    if (i <= n) {
-                        if ((j == n) || (j < n && j > n - i)) {
-                            cout << i;
-                        } else {
-                            cout << " ";
-                        }
+            for ( int i = 1; i <= n; i++ ) {    // prima jumatate
+                for ( int j = 1; j <= 2 * n - 1; j++ ) {
+                    if (( j == n ) || ( j < n && j > n - i ) || ( j > n && j < n + i )) {
+                        cout << i;
+                    } else {
+                        cout << " ";
                     }
                 }
+                
+                cout << endl;
+            }
+
+            for ( int i = n - 1; i >= 1; i-- ) {
+                for ( int j = 1; j <= 2 * n - 1; j++ ) {
+                    if ( ( j == n ) || ( j < n && j > n - i ) || ( j > n && j < n + i ) ) {
+                        cout << i;
+                    } else {
+                        cout << " ";
+                    }
+                }
+
                 cout << endl;
             }
             break;
