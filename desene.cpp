481342c3934887f9187a91be7ca6c97e27efb1a3
Katty Lazar <katy.lazar05@gmail.com>
2021-12-04 15:14:56 +0200

    solved third part of problem - dreptunghiul

--- desene.cpp
+++ desene.cpp
@@ -1,17 +1,13 @@
 #include<iostream>
 using namespace std;
 
-// 111
-// 222
-// 333
-
 int main() {
 
     int c, n;
     cin >> c >> n;
 
     switch ( c ) {
-        case 1:
+        case 1: // done
             // patrat
             for ( int i = 1; i <= n; i++ ) {
                 for ( int j = 1; j <= n; j++ ) {
@@ -22,7 +18,7 @@ int main() {
             }
             break;
 
-        case 2:
+        case 2: // done
             // romb
             for ( int i = 1; i <= n; i++ ) {    // prima jumatate
                 for ( int j = 1; j <= 2 * n - 1; j++ ) {
@@ -36,7 +32,7 @@ int main() {
                 cout << endl;
             }
 
-            for ( int i = n - 1; i >= 1; i-- ) {
+            for ( int i = n - 1; i >= 1; i-- ) {    // a doua jumatate
                 for ( int j = 1; j <= 2 * n - 1; j++ ) {
                     if ( ( j == n ) || ( j < n && j > n - i ) || ( j > n && j < n + i ) ) {
                         cout << i;
@@ -49,9 +45,15 @@ int main() {
             }
             break;
 
-        case 3:
+        case 3: // done
             // dreptunghi -> L = n; l = 2 * n
-            cout << "c is 3";
+            for ( int i = 1; i <= n; i++ ) {
+                for ( int j = 1; j <= 2 * n; j++ ) {
+                    cout << i;
+                }
+
+                cout << endl;
+            }
             break;
 
         case 4:
